3cac46a85b83c95fa7bdfdf39b2a5476537b5de9
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-12-12 15:43:22 +0500

    text changed

--- src/Components/About/About-section/About-section.js
+++ src/Components/About/About-section/About-section.js
@@ -17,7 +17,7 @@ const AboutSection = () => {
             <img src={AboutSectionImg} className="about-section__img" alt="about section img" />
             <div className='about-section__text'>
                 <h2 className='about-section__title' style={coloring}>About this project</h2>
-                <p className='about-section__parag' style={coloring}>First of all, We are Muzaffar and Abdulaziz, who are working on this project. Both of us are adding new stuff for our products permanently. In the future this project is gonna be like <Link to="https://www.amazon.com" className='about-section__link'>Amazon</Link> or <Link to="https://www.alibaba.com" className='about-section__link'>AliBaba</Link>. So that we hope, our Project will be the most high-demanded one!</p>
+                <p className='about-section__parag' style={coloring}>First of all, We are Muzaffar and Abdulaziz, who are working on this project. Both of us are adding new stuffs for our products permanently. In the future this project is gonna be like <Link to="https://www.amazon.com" className='about-section__link'>Amazon</Link> or <Link to="https://www.alibaba.com" className='about-section__link'>AliBaba</Link>. So that we hope, our Project will be the most high-demanded one!</p>
                 <Link to={'/'} className='about-section_btn'>Go to home</Link>
             </div>
         </div>
